d477e0384d404859fec809c7430a8f1631fbad26
Diego Rueda <diego.rueda2@gmail.com>
2024-08-27 20:07:41 -0400

    exceptions

--- backend/backend-task-app/src/main/java/com/diegorueda/backend_task_app/controllers/UserController.java
+++ backend/backend-task-app/src/main/java/com/diegorueda/backend_task_app/controllers/UserController.java
@@ -16,9 +16,6 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-
-
 
 
 @RestController
